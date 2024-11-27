ff86c6f35116876b9dd3f0498bb39d663c72245d
Sugata Jana <vr_sugata.jana@capitaland.com>
2024-11-27 19:12:56 +0530

    make login/signup page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import Login from "./Login";
 import Header from "./Header";
+import Browse from "./Browse";
 
 const Body = () => {
   const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const Body = () => {
     {
       path: "/login",
       element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
     },
   ])
 
